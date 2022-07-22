bc43389ea53accf7696bb52e6fa8102edd6f7ccd
Elizabeth <elizabethoukta@gmail.com>
2022-07-22 21:33:28 +0100

    looopside

---
+++ hamburger.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".hamburger-menu img");
+const closeBtn = document.querySelector(".close-menu");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener("click", openNav);
+closeBtn.addEventListener("click", closeNav);
+
+
+
+
+function openNav(e){
+    e.preventDefault();
+    nav.style.display="block";
+
+
+}
+function closeNav(e){
+    e.preventDefault();
+    nav.style.display ="none"
+}
